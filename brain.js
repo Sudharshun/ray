352d2fa31b05e9a88274d140712e0bb26a920adc
sudharshun@gmail.com <sudharshun@gmail.com>
2017-03-22 23:19:56 -0500

    Initial commit

---
+++ brain.js
@@ -0,0 +1,16 @@
+var brain = {
+    "building": {
+        "page": "README.md",
+        "section": "building"
+    },
+    "translations": {
+        "page": "README.md",
+        "section": "translations"
+    },
+    "gettingstarted": {
+        "page": "README.md",
+        "section": "building"
+    }
+};
+
+module.exports=brain;
