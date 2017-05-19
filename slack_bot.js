ce76e9c820b81ba6771127c0461992ddc2f646af
sudharshun@gmail.com <sudharshun@gmail.com>
2017-05-19 06:53:46 -0500

    clean up and test mehods

--- slack_bot.js
+++ slack_bot.js
@@ -218,13 +218,6 @@ controller.hears(['wakemup'], 'direct_message,direct_mention,mention', function 
 });
 
 
-controller.hears(['test'], 'direct_message,direct_mention,mention', function (bot, message) {
-
-    bot.reply(message,'https://gurukripa.slack.com/files/sudharshun/F4PSKH2LC/Useful_Urls');
-   
-});
-
-
 controller.hears(['uptime', 'identify yourself', 'who are you', 'what is your name'],
     'direct_message,direct_mention,mention', function (bot, message) {
 
